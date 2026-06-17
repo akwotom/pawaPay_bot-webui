1dcb0e63b91397b88683abf26b90cf1eba168288
Son of Binary <akwotom@gmail.com>
2026-06-17 02:31:01 +0100

    Experiment.

--- src/router/index.js
+++ src/router/index.js
@@ -8,10 +8,10 @@
 
 import LiquidationPage from '@/pages/LiquidationPage.vue'
 import PaymentPage from '@/pages/PaymentPage.vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, createMemoryHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createMemoryHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/fund',
