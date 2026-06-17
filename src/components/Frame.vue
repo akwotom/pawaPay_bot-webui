cf0a5423367e6372f80b780f8a100f425caa1be7
Son of Binary <akwotom@gmail.com>
2026-06-17 02:50:19 +0100

    Improved layout and sizing.

--- src/components/Frame.vue
+++ src/components/Frame.vue
@@ -25,8 +25,8 @@
     padding: 2em;
     border-radius: 2em;
     background-color: var(--project-gray-1);
-    min-width: clamp(50px, 500px, 100vw - 3em);
+    min-width: clamp(50px, 500px, 100vw - 15em);
     min-height: clamp(50px, 500px, 100vw - 3em);
-    max-width: clamp(500px, 500px, 100vw - 5em);
+    max-width: clamp(500px, 500px, 100vw - 8em);
 }
 </style>
